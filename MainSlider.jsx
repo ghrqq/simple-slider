2ceb690d117064fe4864772152a336cc56fa65cc
Theo Oz <theoozdev@gmail.com>
2020-11-30 14:10:45 +0300

    Update MainSlider.jsx

--- MainSlider.jsx
+++ MainSlider.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useState } from "react";
 import arrow from "../img/arrow-right.png";
-import bgvideo from "../img/particlebg.mp4";
-import useWindowDimensions from "../tools/useWindowDimensions";
+
+
 
 import logo from "../img/logo.jpg";
 
